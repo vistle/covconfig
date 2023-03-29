6d19418dade209fa6d2b013eb6843b4f0f7a180e
Martin Aumüller <aumuell@reserv.at>
2023-03-29 09:26:33 +0200

    separate headers by directory for installing to proper destination

--- covconfig.cmake
+++ covconfig.cmake
@@ -1,5 +1,6 @@
 set(COVCONFIG_SOURCES ${PREFIX}access.cpp ${PREFIX}array.cpp ${PREFIX}file.cpp ${PREFIX}value.cpp ${PREFIX}detail/base.cpp ${PREFIX}detail/entry.cpp ${PREFIX}detail/logger.cpp ${PREFIX}detail/manager.cpp ${PREFIX}detail/observer.cpp ${PREFIX}detail/output.cpp)
 
-set(COVCONFIG_HEADERS ${PREFIX}access.h ${PREFIX}array.h ${PREFIX}file.h ${PREFIX}value.h ${PREFIX}detail/base.h ${PREFIX}config.h ${PREFIX}detail/entry.h ${PREFIX}detail/export.h ${PREFIX}detail/flags.h ${PREFIX}detail/logger.h ${PREFIX}detail/manager.h ${PREFIX}detail/manager_impl.h ${PREFIX}detail/observer.h ${PREFIX}detail/output.h)
+set(COVCONFIG_HEADERS ${PREFIX}access.h ${PREFIX}array.h ${PREFIX}file.h ${PREFIX}value.h ${PREFIX}config.h ${PREFIX}detail/output.h)
+set(COVCONFIG_DETAIL_HEADERS ${PREFIX}detail/base.h ${PREFIX}detail/entry.h ${PREFIX}detail/export.h ${PREFIX}detail/flags.h ${PREFIX}detail/logger.h ${PREFIX}detail/manager.h ${PREFIX}detail/manager_impl.h ${PREFIX}detail/observer.h ${PREFIX}detail/output.h)
 
 set(COVCONFIG_PRIVATE_INCLUDES ${PREFIX}detail/toml/include)
